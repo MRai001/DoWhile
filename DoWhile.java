8ecd2ef3974888f46be4b0a05b6cace86ea5b255
MRai001 <mallika44@hotmail.com>
2025-04-23 16:06:30 -0500

    Add files via upload

---
+++ DoWhile.java
@@ -0,0 +1,18 @@
+package Day6;
+
+public class DoWhile {
+	
+	public static void main(String[] args) {
+		// print number 1 to 5
+		
+		int i=1; // initialization
+		do
+		{
+			System.out.println("Number: "+i);
+			i=i+1; //Increment
+			
+		} while (i<=5); //condition
+
+	}
+
+}
